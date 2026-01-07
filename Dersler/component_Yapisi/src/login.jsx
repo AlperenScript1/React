a2ea8981858dcf9e9d6b3305d6675b9245317bd5
Alperen <alperensenscript@gmail.com>
2026-01-07 09:55:30 +0300

    7.01.2026

---
+++ Dersler/component_Yapisi/src/login.jsx
@@ -0,0 +1,10 @@
+import React from 'react' //React import ediyoruz.
+
+function login() { //Func oluşturuyoruz
+  return ( // Return ediyor tek farkı bu yoksa normal Func aslında
+    <div>   </div>
+  )
+}
+
+//Başka yerlerde çağırıp kullanabilmek için 
+export default login //Yapıyoruz Func ismi olmalı
